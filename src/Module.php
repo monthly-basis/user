b787e58f126fd9c4f94130a39a9b0e938e3a9f08
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-12-11 14:40:22 -0800

    Add UserTable to service config

--- src/Module.php
+++ src/Module.php
@@ -2,6 +2,7 @@
 namespace LeoGalleguillos\User;
 
 use LeoGalleguillos\User\Model\Service\User as UserService;
+use LeoGalleguillos\User\Model\Table\User as UserTable;
 
 class Module
 {
@@ -11,6 +12,11 @@ class Module
             'factories' => [
                 UserService::class => function ($serviceManager) {
                     return new UserService();
+                },
+                UserTable::class => function ($serviceManager) {
+                    return new UserTable(
+                        $serviceManager->get('main')
+                    );
                 },
             ],
         ];
