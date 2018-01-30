01ceedf0951755a34ac1cf7c71c618634e3cc432
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-01-30 08:59:22 -0800

    Add classes to service manager

--- src/Module.php
+++ src/Module.php
@@ -17,6 +17,9 @@ class Module
     {
         return [
             'factories' => [
+                UserFactory\Post::class => function ($serviceManager) {
+                    return new UserFactory\Post();
+                },
                 UserFactory\User::class => function ($serviceManager) {
                     return new UserFactory\User(
                         $serviceManager->get(UserTable\User::class)
@@ -27,8 +30,19 @@ class Module
                         $serviceManager->get(FlashService\Flash::class)
                     );
                 },
+                UserService\Posts::class => function ($serviceManager) {
+                    return new UserService\Posts(
+                        $serviceManager->get(UserFactory\Post::class),
+                        $serviceManager->get(UserTable\Post::class)
+                    );
+                },
                 UserService\User::class => function ($serviceManager) {
                     return new UserService\User();
+                },
+                UserTable\Post::class => function ($serviceManager) {
+                    return new UserTable\Post(
+                        $serviceManager->get('main')
+                    );
                 },
                 UserTable\Register::class => function ($serviceManager) {
                     return new UserTable\Register(
