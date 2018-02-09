95af16c08e2d30db1ff84105e5acc957850c7bed
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-02-08 18:20:15 -0800

    Update docblock and shortcut

--- src/Model/Service/Photo/Slug.php
+++ src/Model/Service/Photo/Slug.php
@@ -3,14 +3,13 @@ namespace LeoGalleguillos\User\Model\Service\Photo;
 
 use LeoGalleguillos\String\Model\Service as StringService;
 use LeoGalleguillos\User\Model\Entity as UserEntity;
-use LeoGalleguillos\User\Model\Table as UserTable;
 
 class Slug
 {
     /**
      * Construct.
      *
-     * @param UserTable\Photo $photoTable
+     * @param StringService\UrlFriendly $urlFriendlyService
      */
     public function __construct(
         StringService\UrlFriendly $urlFriendlyService
