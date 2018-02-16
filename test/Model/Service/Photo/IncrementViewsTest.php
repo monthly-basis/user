4efd22e3d5ab85d46cbf3268d3c4fef7eda8faa5
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-02-16 07:12:54 -0800

    Create increment views service for photos

---
+++ test/Model/Service/Photo/IncrementViewsTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace LeoGalleguillos\UserTest\Model\Service\Photo;
+
+use LeoGalleguillos\User\Model\Entity as UserEntity;
+use LeoGalleguillos\User\Model\Service as UserService;
+use LeoGalleguillos\User\Model\Table as UserTable;
+use PHPUnit\Framework\TestCase;
+
+class IncrementViewsTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->photoTableMock = $this->createMock(
+            UserTable\Photo::class
+        );
+        $this->incrementViewsService = new UserService\Photo\IncrementViews(
+            $this->photoTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            UserService\Photo\IncrementViews::class,
+            $this->incrementViewsService
+        );
+    }
+}
