11ba86e33db838217546dbb80d5483fee3e0062f
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-01 05:41:00 -0800

    Write unit test for logged in user view helper

---
+++ test/View/Helper/LoggedInUserTest.php
@@ -0,0 +1,41 @@
+<?php
+namespace LeoGalleguillos\UserTest\View\Helper;
+
+use LeoGalleguillos\User\Model\Entity as UserEntity;
+use LeoGalleguillos\User\Model\Service as UserService;
+use LeoGalleguillos\User\View\Helper as UserHelper;
+use PHPUnit\Framework\TestCase;
+
+class LoggedInUserTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->loggedInUserServiceMock = $this->createMock(
+            UserService\LoggedInUser::class
+        );
+        $this->loggedInUserHelper = new UserHelper\LoggedInUser(
+            $this->loggedInUserServiceMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            UserHelper\LoggedInUser::class,
+            $this->loggedInUserHelper
+        );
+    }
+
+    public function testInvoke()
+    {
+        $userEntity = new UserEntity\User();
+        $this->loggedInUserServiceMock->method('getLoggedInUser')->willReturn(
+            $userEntity
+        );
+
+        $this->assertSame(
+            $userEntity,
+            $this->loggedInUserHelper->__invoke()
+        );
+    }
+}
