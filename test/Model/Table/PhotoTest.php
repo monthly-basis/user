2776979055a2951135c3d7fcd0ccfb16aa4efd1d
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-06 08:55:16 -0800

    Write unit test for selecting photos based on user id

--- test/Model/Table/PhotoTest.php
+++ test/Model/Table/PhotoTest.php
@@ -120,4 +120,25 @@ class PhotoTest extends TableTestCase
             $array['title']
         );
     }
+
+    public function testSelectWhereUserId()
+    {
+        $this->photoTable->insert(123, 'jpg', 'title', 'description');
+        $this->photoTable->insert(123, 'png', 'title2', 'description2');
+        $generator = $this->photoTable->selectWhereUserId(123);
+        $this->assertInstanceOf(
+            Generator::class,
+            $generator
+        );
+
+        $this->assertSame(
+            $generator->current()['title'],
+            'title'
+        );
+        $generator->next();
+        $this->assertSame(
+            $generator->current()['title'],
+            'title2'
+        );
+    }
 }
