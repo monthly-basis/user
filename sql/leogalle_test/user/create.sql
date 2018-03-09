017a8420037ef5fa1f7dead9176fc2136c641ca8
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-09 08:31:22 -0800

    Update welcome message to be text field

--- sql/leogalle_test/user/create.sql
+++ sql/leogalle_test/user/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE `user` (
     `user_id` int(10) unsigned auto_increment,
     `username` varchar(255) not null,
     `password_hash` varchar(255) not null,
-    `welcome_message` varchar(255) default null,
+    `welcome_message` text default null,
     `login_hash` varchar(255) default null,
     `views` int unsigned not null default 0,
     `created` datetime not null,
