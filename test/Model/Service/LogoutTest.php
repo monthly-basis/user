a8de26ae2180f181efe6917fb80997e371742d8f
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-03-15 09:01:24 -0700

    Write test for logout service

---
+++ test/Model/Service/LogoutTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace LeoGalleguillos\UserTest\Model\Service;
+
+use LeoGalleguillos\User\Model\Service as UserService;
+use PHPUnit\Framework\TestCase;
+
+class LogoutTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->logoutService = new UserService\Logout();
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            UserService\Logout::class,
+            $this->logoutService
+        );
+    }
+
+    public function testLogout()
+    {
+        $this->assertNull(
+            $this->logoutService->logout()
+        );
+    }
+}
