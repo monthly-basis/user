8085f27cf64d1f7adbeaacdfd2ed78f99bb72741
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-05 19:02:31 -0700

    Create login_log sql files

---
+++ sql/leogalle_test/login_log/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `login_log` (
+    `login_log_id` int(10) unsigned auto_increment,
+    `ip` varchar(255) not null,
+    `success` int unsigned not null,
+    `created` datetime not null,
+    PRIMARY KEY (`login_log_id`),
+    KEY `created` (`created`)
+) charset=utf8;
