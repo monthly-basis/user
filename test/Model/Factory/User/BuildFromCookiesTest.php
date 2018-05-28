dec01f061128ce9daa5f974a86d33315c86024ee
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-28 18:47:20 +0000

    Add test for build from cookies factory

---
+++ test/Model/Factory/User/BuildFromCookiesTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace LeoGalleguillos\UserTest\Model\Factory\User;
+
+use ArrayObject;
+use LeoGalleguillos\Flash\Model\Service as FlashService;
+use LeoGalleguillos\User\Model\Entity as UserEntity;
+use LeoGalleguillos\User\Model\Factory as UserFactory;
+use LeoGalleguillos\User\Model\Service as UserService;
+use LeoGalleguillos\User\Model\Table as UserTable;
+use PHPUnit\Framework\TestCase;
+
+class BuildFromCookiesTest extends TestCase
+{
+    protected function setUp()
+    {
+        $this->logginUserUserServiceMock = $this->createMock(
+            UserService\LoggedInUser::class
+        );
+        $this->userTableMock = $this->createMock(
+            UserTable\User::class
+        );
+        $this->loginHashTableMock = $this->createMock(
+            UserTable\User\LoginHash::class
+        );
+        $this->loginIpTableMock = $this->createMock(
+            UserTable\User\LoginIp::class
+        );
+        $this->buildFromCookiesFactory = new UserFactory\User\BuildFromCookies(
+            $this->logginUserUserServiceMock,
+            $this->userTableMock,
+            $this->loginHashTableMock,
+            $this->loginIpTableMock
+        );
+    }
+
+    public function testInitialize()
+    {
+        $this->assertInstanceOf(
+            UserFactory\User\BuildFromCookies::class,
+            $this->buildFromCookiesFactory
+        );
+    }
+}
