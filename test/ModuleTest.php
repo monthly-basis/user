b02f4008bc6670314ad37dcd31b12640a419facf
Leo Galleguillos <leo.galleguillos@icloud.com>
2019-06-06 05:11:08 +0000

    Add test for controller config

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -12,11 +12,6 @@ class ModuleTest extends TestCase
         $this->module = new Module();
     }
 
-    public function testInstance()
-    {
-        $this->assertInstanceOf(Module::class, $this->module);
-    }
-
     public function testGetConfig()
     {
         $applicationConfig = include(__DIR__ . '/../config/application.config.php');
@@ -44,6 +39,23 @@ class ModuleTest extends TestCase
                     $viewHelperManager->get($class)
                 );
             }
+        }
+    }
+
+    public function testGetControllerConfig()
+    {
+        $applicationConfig = include(__DIR__ . '/../config/application.config.php');
+        $this->application = Application::init($applicationConfig);
+        $serviceManager    = $this->application->getServiceManager();
+        $controllerManager = $serviceManager->get('ControllerManager');
+
+        $controllerConfig  = $this->module->getControllerConfig();
+
+        foreach ($controllerConfig['factories'] as $class => $value) {
+            $this->assertInstanceOf(
+                $class,
+                $controllerManager->get($class)
+            );
         }
     }
 
