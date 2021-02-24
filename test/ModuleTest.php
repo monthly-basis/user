0e8313d617f04faf2d59b9f798afc3b7ba0bdee6
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-02-24 19:02:45 +0000

    Update ModuleTest to extend ModuleTestCase

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -1,45 +1,15 @@
 <?php
 namespace MonthlyBasis\UserTest;
 
+use MonthlyBasis\LaminasTest\ModuleTestCase;
 use MonthlyBasis\User\Module;
-use PHPUnit\Framework\TestCase;
 use Laminas\Mvc\Application;
 
-class ModuleTest extends TestCase
+class ModuleTest extends ModuleTestCase
 {
     protected function setUp(): void
     {
         $this->module = new Module();
-    }
-
-    public function testGetConfig()
-    {
-        $applicationConfig = include(__DIR__ . '/../config/application.config.php');
-        $this->application = Application::init($applicationConfig);
-        $serviceConfig     = $this->module->getServiceConfig();
-        $serviceManager    = $this->application->getServiceManager();
-        $viewHelperManager = $serviceManager->get('ViewHelperManager');
-        $config            = $this->module->getConfig();
-
-        $this->assertTrue(is_array($config));
-
-        if (isset($config['view_helpers']['aliases'])) {
-            foreach ($config['view_helpers']['aliases'] as $alias => $class) {
-                $this->assertInstanceOf(
-                    $class,
-                    $viewHelperManager->get($class)
-                );
-            }
-        }
-
-        if (isset($config['view_helpers']['factories'])) {
-            foreach ($config['view_helpers']['factories'] as $class => $value) {
-                $this->assertInstanceOf(
-                    $class,
-                    $viewHelperManager->get($class)
-                );
-            }
-        }
     }
 
     public function testGetControllerConfig()
@@ -55,21 +25,6 @@ class ModuleTest extends TestCase
             $this->assertInstanceOf(
                 $class,
                 $controllerManager->get($class)
-            );
-        }
-    }
-
-    public function testGetServiceConfig()
-    {
-        $applicationConfig = include(__DIR__ . '/../config/application.config.php');
-        $this->application = Application::init($applicationConfig);
-        $serviceConfig     = $this->module->getServiceConfig();
-        $serviceManager    = $this->application->getServiceManager();
-
-        foreach ($serviceConfig['factories'] as $class => $value) {
-            $this->assertInstanceOf(
-                $class,
-                $serviceManager->get($class)
             );
         }
     }
