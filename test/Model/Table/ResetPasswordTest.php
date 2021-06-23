4cda4d6eaf2806e208aae3b1973833aa9776f957
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-06-23 22:50:56 +0000

    Modernize unit test for table model

--- test/Model/Table/ResetPasswordTest.php
+++ test/Model/Table/ResetPasswordTest.php
@@ -2,37 +2,16 @@
 namespace MonthlyBasis\UserTest\Model\Table;
 
 use MonthlyBasis\User\Model\Table as UserTable;
-use MonthlyBasis\UserTest\TableTestCase;
-use PHPUnit\Framework\TestCase;
-use Laminas\Db\Adapter\Adapter;
+use MonthlyBasis\LaminasTest\TableTestCase;
 
 class ResetPasswordTest extends TableTestCase
 {
-    /**
-     * @var string
-     */
-    protected $sqlPath;
-
     protected function setUp(): void
     {
-        $this->sqlPath       = $_SERVER['PWD'] . '/sql/test/reset_password/';
-        $this->adapter       = $this->getAdapter();
-        $this->resetPasswordTable = new UserTable\ResetPassword($this->adapter);
-
-        $this->dropTable();
-        $this->createTable();
-    }
-
-    protected function dropTable()
-    {
-        $sql = file_get_contents($this->sqlPath . 'drop.sql');
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    protected function createTable()
-    {
-        $sql = file_get_contents($this->sqlPath . 'create.sql');
-        $result = $this->adapter->query($sql)->execute();
+        $this->dropAndCreateTable('reset_password');
+        $this->resetPasswordTable = new UserTable\ResetPassword(
+            $this->getAdapter()
+        );
     }
 
     public function testInitialize()
