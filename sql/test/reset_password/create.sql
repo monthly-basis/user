c38ea4424852edea551f4fb22278081fb805a58f
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-06-23 19:23:15 +0000

    Add `accessed` and `used` columns to `reset_password` table

--- sql/test/reset_password/create.sql
+++ sql/test/reset_password/create.sql
@@ -3,6 +3,8 @@ CREATE TABLE `reset_password` (
     `user_id` int(10) unsigned not null,
     `code` varchar(32) not null,
     `created` datetime not null,
+    `accessed` datetime DEFAULT NULL,
+    `used` datetime DEFAULT NULL,
     PRIMARY KEY (`reset_password_id`),
     KEY (`code`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
