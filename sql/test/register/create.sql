1ed379f86b1b7ad738449d5ff71cc4eeeaeb5d99
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-07 03:55:45 +0000

    Add `register`.`created` column with DEFAULT CURRENT_TIMESTAMP

--- sql/test/register/create.sql
+++ sql/test/register/create.sql
@@ -7,6 +7,7 @@ CREATE TABLE `register` (
     `birthday` DATETIME NOT NULL,
     `gender` CHAR(1) DEFAULT NULL,
     `activated` tinyint(1) unsigned NOT NULL DEFAULT '0',
+    `created` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`register_id`),
     UNIQUE KEY `register_id_activation_code` (`register_id`,`activation_code`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
