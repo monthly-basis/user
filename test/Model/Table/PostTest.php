b48472a246cad7c13f03b3933b46810bd56efd8b
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-18 09:40:34 +0000

    Modernize unit test for `post` table

--- test/Model/Table/PostTest.php
+++ test/Model/Table/PostTest.php
@@ -2,55 +2,21 @@
 namespace MonthlyBasis\UserTest\Model\Table;
 
 use ArrayObject;
+use MonthlyBasis\LaminasTest\TableTestCase;
 use MonthlyBasis\User\Model\Table as UserTable;
-use MonthlyBasis\UserTest\TableTestCase;
-use Laminas\Db\Adapter\Adapter;
-use PHPUnit\Framework\TestCase;
 use Laminas\Db\Adapter\Exception\InvalidQueryException;
 
 class PostTest extends TableTestCase
 {
-    /**
-     * @var string
-     */
-    protected $sqlPath = __DIR__ . '/../../..' . '/sql/test/post/';
-
     protected function setUp(): void
     {
-        $configArray     = require(__DIR__ . '/../../../config/autoload/local.php');
-        $configArray     = $configArray['db']['adapters']['test'];
-        $this->adapter   = new Adapter($configArray);
-
         $this->setForeignKeyChecks0();
-        $this->dropTables();
-        $this->createTables();
+        $this->dropTables(['post', 'user']);
+        $this->createTables(['post', 'user']);
         $this->setForeignKeyChecks1();
 
-        $this->postTable = new UserTable\Post($this->adapter);
-        $this->userTable = new UserTable\User($this->adapter);
-    }
-
-    protected function dropTables()
-    {
-        $sql = file_get_contents($this->sqlDirectory . '/test/user/drop.sql');
-        $result = $this->adapter->query($sql)->execute();
-
-        $sql = file_get_contents($this->sqlDirectory . '/test/post/drop.sql');
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    protected function createTables()
-    {
-        $sql = file_get_contents($this->sqlDirectory . '/test/user/create.sql');
-        $result = $this->adapter->query($sql)->execute();
-
-        $sql = file_get_contents($this->sqlDirectory . '/test/post/create.sql');
-        $result = $this->adapter->query($sql)->execute();
-    }
-
-    public function testInitialize()
-    {
-        $this->assertInstanceOf(UserTable\Post::class, $this->postTable);
+        $this->postTable = new UserTable\Post($this->getAdapter());
+        $this->userTable = new UserTable\User($this->getAdapter());
     }
 
     public function testInsert()
