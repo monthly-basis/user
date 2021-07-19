4980937ecbd21483a797ad5b1e71324e7ec0dc51
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-19 23:50:29 +0000

    Write method to select where `ip_address` and `created` greater than

--- src/Model/Table/RegisterNotOldEnoughLog.php
+++ src/Model/Table/RegisterNotOldEnoughLog.php
@@ -1,7 +1,9 @@
 <?php
 namespace MonthlyBasis\User\Model\Table;
 
+use DateTime;
 use Laminas\Db\Adapter\Driver\Pdo\Result;
+use Laminas\Db\Sql\Predicate\Operator;
 use MonthlyBasis\User\Model\Db as UserDb;
 
 class RegisterNotOldEnoughLog
@@ -22,5 +24,25 @@ class RegisterNotOldEnoughLog
              ])
              ;
         return $this->sql->prepareStatementForSqlObject($insert)->execute();
+    }
+
+    public function selectWhereIpAddressAndCreatedGreaterThan(
+        string $ipAddress,
+        DateTime $created
+    ): Result {
+        $select = $this->sql
+            ->select()
+            ->columns([
+                'register_not_old_enough_log_id',
+                'ip_address',
+                'created',
+            ])
+            ->from('register_not_old_enough_log')
+            ;
+        $select->where
+            ->equalTo('ip_address', $ipAddress)
+            ->greaterThanOrEqualTo('created', $created->format('Y-m-d H:i:s'))
+            ;
+        return $this->sql->prepareStatementForSqlObject($select)->execute();
     }
 }
--- test/Model/Table/RegisterNotOldEnoughLogTest.php
+++ test/Model/Table/RegisterNotOldEnoughLogTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace MonthlyBasis\UserTest\Model\Table;
 
+use DateTime;
 use MonthlyBasis\LaminasTest\TableTestCase;
 use MonthlyBasis\User\Model\Db as UserDb;
 use MonthlyBasis\User\Model\Table as UserTable;
@@ -31,5 +32,60 @@ class RegisterNotOldEnoughLogTest extends TableTestCase
             1,
             $result->getAffectedRows()
         );
+
+        $result = $this->registerNotOldEnoughLogTable->selectWhereIpAddressAndCreatedGreaterThan(
+            '1.2.3.4',
+            (new DateTime())->modify('-1 day')
+        );
+        $array = $result->current();
+        $this->assertSame(
+            [
+                'register_not_old_enough_log_id' => '1',
+                'ip_address'                     => '1.2.3.4',
+            ],
+            [
+                'register_not_old_enough_log_id' => $array['register_not_old_enough_log_id'],
+                'ip_address'                     => $array['ip_address']
+            ]
+        );
+    }
+
+    public function test_selectWhereIpAddressAndCreatedGreaterThan()
+    {
+        $result = $this->registerNotOldEnoughLogTable->selectWhereIpAddressAndCreatedGreaterThan(
+            '5.6.7.8',
+            (new DateTime())->modify('-1 day')
+        );
+        $this->assertEmpty($result);
+
+        $this->registerNotOldEnoughLogTable->insert('5.6.7.8');
+
+        $result = $this->registerNotOldEnoughLogTable->selectWhereIpAddressAndCreatedGreaterThan(
+            '5.6.7.8',
+            (new DateTime())->modify('-1 day')
+        );
+        $array = $result->current();
+        $this->assertSame(
+            [
+                'register_not_old_enough_log_id' => '1',
+                'ip_address'                     => '5.6.7.8',
+            ],
+            [
+                'register_not_old_enough_log_id' => $array['register_not_old_enough_log_id'],
+                'ip_address'                     => $array['ip_address']
+            ]
+        );
+
+        $result = $this->registerNotOldEnoughLogTable->selectWhereIpAddressAndCreatedGreaterThan(
+            '1.2.3.4',
+            (new DateTime())->modify('-1 day')
+        );
+        $this->assertEmpty($result);
+
+        $result = $this->registerNotOldEnoughLogTable->selectWhereIpAddressAndCreatedGreaterThan(
+            '5.6.7.8',
+            (new DateTime())->modify('+1 day')
+        );
+        $this->assertEmpty($result);
     }
 }
