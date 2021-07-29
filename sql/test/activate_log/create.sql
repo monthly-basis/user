3596b926ea06430a010e2799caeb94f26940bde4
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-29 21:33:36 +0000

    Add KEY on (`ip_address`,`success`) columns

--- sql/test/activate_log/create.sql
+++ sql/test/activate_log/create.sql
@@ -3,5 +3,6 @@ CREATE TABLE `activate_log` (
     `ip_address` varchar(45) NOT NULL,
     `success` tinyint(1) unsigned NOT NULL,
     `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`activate_log_id`)
+    PRIMARY KEY (`activate_log_id`),
+    KEY `ip_address_success` (`ip_address`, `success`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
