119d2cea8dde753b9c42668d650d1ebf0ce60f54
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-07-29 19:09:09 +0000

    Write SQL files for `activate_log` table

---
+++ sql/test/activate_log/create.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `activate_log` (
+    `activate_log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `ip_address` varchar(45) NOT NULL,
+    `success` tinyint(1) unsigned NOT NULL,
+    `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`activate_log_id`)
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
