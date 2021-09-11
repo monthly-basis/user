0fb66d986fccba0f51bb4b5a8984a286b08dee98
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-09-11 04:39:59 +0000

    If user is not logged in, redirect to login page

--- src/Controller/Account/ChangePassword.php
+++ src/Controller/Account/ChangePassword.php
@@ -2,11 +2,24 @@
 namespace MonthlyBasis\User\Controller\Account;
 
 use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\Mvc\MvcEvent;
+use MonthlyBasis\User\Model\Service as UserService;
 
 class ChangePassword extends AbstractActionController
 {
     public function __construct(
+        UserService\LoggedIn $loggedInService
     ) {
+        $this->loggedInService = $loggedInService;
+    }
+
+    public function onDispatch(MvcEvent $mvcEvent)
+    {
+        if (!$this->loggedInService->isLoggedIn()) {
+            return $this->redirect()->toRoute('login')->setStatusCode(303);
+        }
+
+        return parent::onDispatch($mvcEvent);
     }
 
     public function changePasswordAction()
--- src/Module.php
+++ src/Module.php
@@ -197,7 +197,9 @@ class Module
         return [
             'factories' => [
                 UserController\Account\ChangePassword::class => function ($sm) {
-                    return new UserController\Account\ChangePassword();
+                    return new UserController\Account\ChangePassword(
+                        $sm->get(UserService\LoggedIn::class),
+                    );
                 },
                 UserController\Activate::class => function ($sm) {
                     return new UserController\Activate(
