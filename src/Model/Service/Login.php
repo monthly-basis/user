0b0413bfa9c36093c41a9c32d51b8ebd95dec728
Leo Galleguillos <leo.galleguillos@icloud.com>
2021-10-20 05:11:19 +0000

    Revert "Calculate expires value before setting cookies"
    This reverts commit a3002fcab0c3b74fd8eaa227cb0a6578a90fca7a.

--- src/Model/Service/Login.php
+++ src/Model/Service/Login.php
@@ -62,7 +62,6 @@ class Login
         $loginHash  = $this->randomService->getRandomString(64);
         $loginIp    = $_SERVER['REMOTE_ADDR'];
         $httpsToken = $this->randomService->getRandomString(64);
-        $expires    = empty($_POST['keep']) ? 0 : time() + 30 * 24 * 60 * 60;
 
         $this->loginDateTimeTable->updateSetToNowWhereUserId(
             $userEntity->getUserId()
@@ -81,7 +80,6 @@ class Login
             $userEntity,
             $loginHash,
             $httpsToken,
-            $expires,
         );
 
         return true;
@@ -90,11 +88,10 @@ class Login
     protected function setCookies(
         UserEntity\User $userEntity,
         string $loginHash,
-        string $httpsToken,
-        int $expires
+        string $httpsToken
     ) {
         $options = [
-            'expires'  => $expires,
+            'expires'  => empty($_POST['keep']) ? 0 : time() + 30 * 24 * 60 * 60,
             'path'     => '/',
             'domain'   => $_SERVER['HTTP_HOST'],
             'secure'   => true,
