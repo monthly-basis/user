766d0842ac9d852d6f61dd80c3caf63e85c7288f
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-07-26 15:38:01 +0000

    Set default `created` to current timestamp

--- sql/test/user_follow/create.sql
+++ sql/test/user_follow/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `user_follow` (
     `user_id_1` int(10) unsigned NOT NULL,
     `user_id_2` int(10) unsigned NOT NULL,
-    `created` datetime NOT NULL,
+    `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`user_id_1`, `user_id_2`),
     KEY `user_id_2` (`user_id_2`),
     KEY `created` (`created`)
