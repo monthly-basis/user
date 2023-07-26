6bdb2bb1c1fd53536a165402d28e04e7479b4332
Leo Galleguillos <leo.galleguillos@icloud.com>
2023-07-26 15:25:00 +0000

    Create `user_follow` sql files

---
+++ sql/test/user_follow/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user_follow` (
+    `user_id_1` int(10) unsigned NOT NULL,
+    `user_id_2` int(10) unsigned NOT NULL,
+    `created` datetime NOT NULL,
+    PRIMARY KEY (`user_id_1`, `user_id_2`),
+    KEY `user_id_2` (`user_id_2`),
+    KEY `created` (`created`)
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
